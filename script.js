d33c0288a3a555499453d13808dd98ae13678957
eagle173 <jazzgiant9@gmail.com>
2025-03-20 21:53:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ document.getElementById('add-item').addEventListener('click', function() {
         <label for="item-type">항목 유형:</label>
         <select class="item-type" onchange="updateForm(this)">
             <option value="">선택하세요</option>
-            <option value="type1">유형 1</option>
-            <option value="type2">유형 2</option>
+            <option value="type1">영혼각차</option>
+            <option value="type2">초도</option>
         </select>
         <div class="item-details"></div>
     `;
@@ -22,13 +22,35 @@ function updateForm(selectElement) {
 
     if (selectElement.value === 'type1') {
         itemDetails.innerHTML = `
-            <label for="detail1">유형 1 세부사항:</label>
+            <label for="detail1">영혼각차 세부사항:</label>
             <input type="text" id="detail1" name="detail1">
         `;
     } else if (selectElement.value === 'type2') {
         itemDetails.innerHTML = `
-            <label for="detail2">유형 2 세부사항:</label>
+            <label for="detail2">초도 세부사항:</label>
             <input type="text" id="detail2" name="detail2">
         `;
     }
 }
+
+document.getElementById('type').addEventListener('change', function() {
+    const formItems = document.getElementById('form-items');
+    formItems.innerHTML = ''; // 기존 항목 초기화
+
+    if (this.value === 'type1') {
+        formItems.innerHTML = `
+            <label for="session">회차:</label>
+            <input type="text" id="session" name="session" required>
+            <br>
+            <label for="address">주소:</label>
+            <input type="text" id="address" name="address" required>
+            <br>
+        `;
+    } else if (this.value === 'type2') {
+        formItems.innerHTML = `
+            <label for="target">초도대상:</label>
+            <input type="text" id="target" name="target" required>
+            <br>
+        `;
+    }
+});
